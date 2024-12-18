5339d4a0164e52a2985f753bf28b233d60b6a2e7
RTerabyte <rodry.ter@gmail.com>
2024-12-18 20:33:29 +0100

    configurando  y comprobando que funciona

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hola ap4 PI");
+// creamos la constante express
+const express = require("express");
+
+// creamos otra constante que inicie el modulo express
+const app = express();
+
+//configuramos el puerto de entrada
+// process.env.PORT sirve para que eliga el puerto que vamos a utilizar.
+app.set("port", process.env.PORT || 8080);
+
+//con esto detectaremos si funciona el puerto si realmente alguien a entrado.
+app.listen(app.get("port"), leerPuerto());
+
+// creamos la funcion leerPuerto
+function leerPuerto() {
+  console.log(`hola desde el puerto ${app.get("port")}`);
+}
