d4aeef89e076d0779cca9fd5cecf2d5741aef28e
RTerabyte <rodry.ter@gmail.com>
2024-12-18 23:21:15 +0100

    Rutas de prueba configuradas para que funcionen

--- src/index.js
+++ src/index.js
@@ -8,20 +8,7 @@ const app = express();
 // process.env.PORT sirve para que eliga el puerto que vamos a utilizar.
 app.set("port", process.env.PORT || 8080);
 
-//con esto detectaremos si funciona el puerto si realmente alguien a entrado.
-app.listen(app.get("port"), leerPuerto());
-
-// creamos la funcion leerPuerto
-function leerPuerto() {
+//iniciando el servidor
+app.listen(app.get("port"), function () {
   console.log(`hola desde el puerto ${app.get("port")}`);
-}
-//creamos una ruta get
-app.get("/rutaGet", function (req, res) {
-  console.log("alguen a accedido a la ruta get");
-  res.send("hola haciendo pruebas desde la ruta get");
-});
-
-app.get("/", function (req, res) {
-  console.log("alguen a accedido al servidor");
-  res.send("Bienvenido al servidor");
 });
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+// importamos el router, lo llamamos y lo guardamos en una variable
+const { Router } = require("express");
+const router = Router();
+
+// ahora en vez de utilizar app.get usaremos la variable router
+router.get("/rutaGetConRouter", function (req, res) {
+  console.log("Alguien a accedido a la ruta desde la carpeta routes");
+  res.send("Hola desde index.js de la carpeta routes");
+});
+
+// lo exportamos con este comando
+module.exports = routes;
